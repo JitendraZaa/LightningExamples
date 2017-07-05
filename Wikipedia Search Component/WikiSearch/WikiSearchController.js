8ef01c694152c88a98cdea6634ea2866790c97e5
Jitendra Zaa <jitendra.zaa@gmail.com>
2017-07-05 08:59:35 -0400

    Fixed defect to support enter button pressed event

--- Wikipedia Search Component/WikiSearch/WikiSearchController.js
+++ Wikipedia Search Component/WikiSearch/WikiSearchController.js
@@ -1,6 +1,8 @@
 ({
 	searchWiki : function(component, event, helper) {
-        if(event.ea.keyCode == 13){ 
+        if((event.ea && event.ea.keyCode == 13) || 
+			(event.getParams() && event.getParams().keyCode == 13) ||
+			(event.keyCode == 13)){ 
             helper.wikiCallOut(component.get('v.txtWikiText'),component, helper); 
         }
 	}
